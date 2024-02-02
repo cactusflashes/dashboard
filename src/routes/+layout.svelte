3351e833fcf3b07ee50800aa12d6a016e62681ad
Joseph <121457318+cactusflashes@users.noreply.github.com>
2024-02-01 19:50:34 -0700

    first pass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<style>
+    @import url('./style.css'); 
+</style>
+
+<script>
+        import Sidebar from './sidebar.svelte'; 
+</script>
+
+<Sidebar />
+<slot /> 
---
+++ src/routes/sidebar.svelte
@@ -0,0 +1,99 @@
+<script>
+    //time and formatting
+    var currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    var formattedHours = hours < 10 ? hours : hours;
+    var formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    var formattedTime = formattedHours + ":" + formattedMinutes + " " + amPm;
+
+    //day of the week
+    var currentDate = new Date();
+    var dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    var currentDayString = dayNames[currentDate.getDay()];
+
+    //dynamic greeting
+    function getGreeting() {
+        let currentHour = new Date().getHours();
+
+        if (currentHour >= 5 && currentHour < 12) {
+            return "morning";
+        } else if (currentHour >= 12 && currentHour < 18) {
+            return "afternoon";
+        } else if (currentHour >= 18 && currentHour < 21) {
+            return "evening";
+        }
+    }
+
+    let greeting = getGreeting();
+
+    //weather
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.1987522&lon=-111.6518229&appid=e28dc6acd1bfed7577b41d518f0151b5&units=imperial`;
+    let currentTemp;
+    let currentConditions;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+
+            currentTemp = data.main.temp.toFixed(0);
+            currentConditions = data.weather[0].description;
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+
+
+</script>
+
+
+<div class="sidebar">
+    <div class="sidebar-text">
+        <h2>{formattedTime}</h2>
+        <h3 style="margin: 0; opacity: 0.6;"> {currentDayString} </h3>
+        <h2>Good {greeting}, Joseph.</h2>
+        <h3>It's currently {currentTemp}º with {currentConditions} this {greeting}.</h3>
+    </div>
+</div>
+
+<style>
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        height: 80%;
+        width: 250px;
+        top: 10%;
+        left: 5%;
+        border-radius: 20px;
+        -webkit-backdrop-filter: blur(10px);
+        backdrop-filter: blur(10px);
+        background-color: rgba(255, 255, 255, 0.2);
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        align-items: center;
+        justify-content: center;
+    }
+
+    .sidebar-text {
+        display: flex;
+        flex-direction: column;
+        height: 95%;
+        width: 80%;
+    }
+
+    @media all and (max-width: 700px) {
+        .sidebar {
+            left: 15%;
+        }
+    }
+</style>
